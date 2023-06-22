ccaa0c6a70b1e25283a4fa226f2765edcfece8e3
Alvaro Navarro <128388898+alvarowops@users.noreply.github.com>
2023-06-21 23:52:05 -0400

    Update menu.php

--- menu.php
+++ menu.php
@@ -7,7 +7,7 @@
 <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
   <ul class="nav navbar-nav">
     <li>
-      <a href="load.php">Load Test</a>
+      <a href="load.php">Stress Test</a>
     </li>
     <li>
       <a href="rds.php">RDS</a>
